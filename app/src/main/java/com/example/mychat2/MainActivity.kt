9abebd9a7ff6c9b57c6638aaac7ec7d4fc401bdb
GnomGrom97 <77610135+GnomGrom97@users.noreply.github.com>
2023-10-17 02:19:11 +0300

    Полетело все по пизде:делаю снова

--- app/src/main/java/com/example/mychat2/MainActivity.kt
+++ app/src/main/java/com/example/mychat2/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.mychat2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //подключение к бд
         setContentView(R.layout.activity_main)
+
+
         val database = Firebase.database
         //записываем в бд message:hello...
         val myRef = database.getReference("message")
